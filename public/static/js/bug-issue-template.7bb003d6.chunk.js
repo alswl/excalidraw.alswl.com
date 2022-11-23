734ce59a9c7086095da778ba85ca120608e62dcd
alswl <alswlx@gmail.com>
2022-11-23 22:18:50 +0800

    feat: using public dir, update with release/v0.13.0-fork

---
+++ public/static/js/bug-issue-template.7bb003d6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpexcalidraw=this.webpackJsonpexcalidraw||[]).push([[1],{331:function(n,e,t){"use strict";t.r(e),e.default=function(n){return"\n### Scene content\n\n```\nPaste scene content here\n```\n\n### Sentry Error ID\n\n".concat(n,"\n")}}}]);
+//# sourceMappingURL=bug-issue-template.7bb003d6.chunk.js.map
--- static/js/socketIoClient.1b8405dd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpexcalidraw=this.webpackJsonpexcalidraw||[]).push([[52],{322:function(a,c){}}]);
-//# sourceMappingURL=socketIoClient.1b8405dd.chunk.js.map
